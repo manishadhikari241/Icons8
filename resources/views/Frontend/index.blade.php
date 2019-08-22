d4bc41e89333fc1e83dbca797c3c45ced31be3ae
manishadhikari241 <manishadhikari241@gmail.com>
2019-08-22 19:40:56 +0545

    ok

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -40,6 +40,8 @@
                         <ul>
                             <li ><a href="photocreator/index.html" >Photo Creator</a></li>
                             <li ><a href="{{route('photo')}}" >Photo library</a></li>
+                            <li ><a href="{{route('photo')}}" >Photo library</a></li>
+                            <li ><a href="{{route('photo')}}" >Photo library</a></li>
                         </ul>
                     </div>
                 </li>
