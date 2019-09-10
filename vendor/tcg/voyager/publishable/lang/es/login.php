7b8bc09ddc24f85e4aa1fbcd00031fb4a50dc3f4
manishadhikari241 <manishadhikari241@gmail.com>
2019-09-10 13:25:11 +0545

    Remove the now ignored directory "vendor"

--- vendor/tcg/voyager/publishable/lang/es/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'loggingin'    => 'Iniciando sesión',
-    'signin_below' => 'Ingresar abajo:',
-    'welcome'      => 'Bienvenido a Voyager. El administrador que le faltaba a Laravel ',
-];
